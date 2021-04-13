e390cb53b205bc8054e906487023c72c7c430195
Ersan Ercek <cinoersan@gmail.com>
2021-04-13 08:49:57 +0100

    Add a small description to movement logic

--- Nasa.Business/Extensions/RoverExtensions.cs
+++ Nasa.Business/Extensions/RoverExtensions.cs
@@ -16,12 +16,15 @@ namespace Nasa.Business.Extensions
 
         private static void Turn(this RoverStatus status, string movementKey)
         {
+            // Movements are index based, turning right or left changes the index of the heading
+            // Each index denotes one heading starting from north
             var movement = LocationKeys.Movements.FirstOrDefault(t => t.Key.Equals(movementKey));
             var index = LocationKeys.Directions.FindIndex(t => t.Equals(status.Heading)) + (movement?.Increment ?? 0);
             status.Heading = LocationKeys.Directions[(index + 4) % 4];
         }
         private static void MoveForward(this RoverStatus status)
         {
+            // the increments are determined according to the index of current heading.
             var index = LocationKeys.Directions.FindIndex(t => t.Equals(status.Heading));
             var movement = LocationKeys.Increments[index];
             status.CurrentCoordinate.X += movement.IncrementX;
